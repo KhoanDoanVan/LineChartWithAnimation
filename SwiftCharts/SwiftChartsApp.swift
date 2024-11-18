fb17d089f19a5549a33538b5e3d467111faecece
khoanisdev <doanvankhoan124@gmail.com>
2024-11-18 20:01:11 +0700

    Initial Commit

---
+++ SwiftCharts/SwiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsApp.swift
+//  SwiftCharts
+//
+//  Created by Đoàn Văn Khoan on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
